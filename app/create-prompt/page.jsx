de1533abe57b10c82bddc5833b208918d2223479
fbonilla <franciscobonillac@gmail.com>
2023-07-25 10:43:22 -0500

    create form styles

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -1,16 +1,26 @@
-'use client'
-import Form from "@components/Form"
-import { useSession } from "next-auth/react"
-import { useRouter } from "next/router"
-import { useState } from "react"
-
-
-
+"use client";
+import Form from "@components/Form";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import { useState } from "react";
 
 const CreatePrompt = () => {
-  return (
-    <Form>CreatePrompt</Form>
-  )
-}
+  const [submitting, setSubmitting] = useState(false);
+  const [post, setPost] = useState({
+    prompt: "",
+    tag: "",
+  });
 
-export default CreatePrompt
+  const CreatePrompt = async (e) => {};
+  return (
+    <Form
+      type="Create"
+      post={post}
+      setPost={setPost}
+      submitting={submitting}
+      handleSubmit={CreatePrompt}
+    />
+  );
+};
+
+export default CreatePrompt;
