5bb85a84072f9cf0ac105fa3b55c7159927ce4c1
fbonilla <franciscobonillac@gmail.com>
2023-07-25 17:00:39 -0500

    fix span in the landing page

--- app/page.jsx
+++ app/page.jsx
@@ -6,7 +6,7 @@ const page = () => {
       <h1 className="head_text text-center">
         Discover & Share
         <br className="max-md:hidden" />
-        <span className="orange_gradient text-center"> AI-Powred Promps</span>
+        <span className="orange_gradient text-center"> AI-Powered Prompts</span>
       </h1>
       <p className="desc text-center">
         Promptopia is an open-source A1 prompting tool for modern world to
