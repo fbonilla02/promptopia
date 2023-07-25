ac371597722af5f91d941c544005b875146836f1
fbonilla <franciscobonillac@gmail.com>
2023-07-25 10:22:46 -0500

    create prompt  component

---
+++ app/create-prompt/page.jsx
@@ -0,0 +1,16 @@
+'use client'
+import Form from "@components/Form"
+import { useSession } from "next-auth/react"
+import { useRouter } from "next/router"
+import { useState } from "react"
+
+
+
+
+const CreatePrompt = () => {
+  return (
+    <Form>CreatePrompt</Form>
+  )
+}
+
+export default CreatePrompt
